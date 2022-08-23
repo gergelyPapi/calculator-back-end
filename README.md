a987bb95715dcc011f580192e1f99109b41f0484
Gergely Papi <gergely.papi.cc@gmail.com>
2022-08-23 23:37:00 +0200

    Added backed for calc app Load/Save GET/POST request endpoints Database in form of a txt file, saved locally Payload back on POST request is parsed in a CVS format Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # calculator-back-end
+
+## Description
+
+Creating an back-end for calc app for recruitment purposes.
+
+### `Calculator`
+
+- REST API for solving load/save of results coming from calculator front-end app
+- https://github.com/gergelyPapi/calculator-front-end
---
+++ nodeServer.js
@@ -0,0 +1,57 @@
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const parse = (csvString) => {
+    const parsedCsv = csvString
+    .toString()
+    .split('\n')
+    .map(e => e.trim())
+    .map(e => e.split(',').map(e => e.trim()))[0];
+    return parsedCsv[parsedCsv.length - 2];
+}
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}
+const port = 4200;
+const {
+    writeFile,
+    readFile
+} = require('fs').promises
+const app = express();
+
+app.use(express.static(__dirname + '/DB'));
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+app.use(bodyParser.json());
+
+app.get('/load', async (req, res) => {
+    console.log('LOAD');
+    try {
+        const data = await readFile('./calculatorDataBase.txt', {
+            encoding: 'utf8',
+        });
+        return res.status(200).json({ result: parse(data) });
+    } catch (err) {
+        return res.status(500);
+    }
+});
+
+app.post('/save', async (req,res) => {
+    console.log('SAVE');
+    const {
+        numToSave
+    } = req.body;
+    try {
+        const content = numToSave + ',';
+        await writeFile('./calculatorDataBase.txt', content, { flag: 'a+' });
+        return res.status(200).json({});
+    } catch (err) {
+        return res.status(500);
+    }
+});
+
+app.listen(port, () => console.log(`Running on localhost:${port}`));
